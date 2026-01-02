a5ed88960a823e623c5ddca961711a3c4478fa55
Harshit_Parmar <parmarharshit441@gmail.com>
2026-01-02 11:03:12 +0530

    standarized errors and responses from the server side !

---
+++ server/src/utils/apiError.ts
@@ -0,0 +1,27 @@
+interface ApiErrorInterface {
+  statusCode: number;
+  message: string;
+  error: any;
+  stack?: string;
+}
+
+class ApiError extends Error implements ApiErrorInterface {
+  statusCode: number;
+  message: string;
+  error: any;
+  stack?: string;
+
+  constructor(statusCode: number, message: string, error: any, stack?: string) {
+    super(message);
+    this.statusCode = statusCode;
+    this.message = message;
+    this.error = error;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export default ApiError;
---
+++ server/src/utils/asyncHandler.ts
@@ -0,0 +1,17 @@
+import type { Request, Response, NextFunction } from "express";
+
+type AsyncHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<any> | any;
+
+const asyncHandler = (requestHandler: AsyncHandler) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+export default asyncHandler;
